930b2510842e0b894c22fceb67ba02fe3615344e
KimSoo <ilppang@naver.com>
2022-09-02 12:42:44 +0900

    Auth 폴더 구조 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './—no-spec/auth/auth.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
-    BoardsModule],  
+    BoardsModule,
+    AuthModule],
+  controllers: [AuthController],  
 })
 export class AppModule {}
