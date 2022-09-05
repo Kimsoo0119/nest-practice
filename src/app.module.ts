4490ce1677973220f4e6dd4aeee2de677b0b23d7
KimSoo <ilppang@naver.com>
2022-09-05 15:00:27 +0900

    유저 회원가입 기능, DTO, Validation 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
-import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
-  controllers: [AuthController],
 })
 export class AppModule {}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,13 @@
-import { Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthcredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 
-export class UserRepository extends Repository<User> {}
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthcredentialDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    await this.save(user);
+  }
+}
