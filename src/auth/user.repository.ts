d7a3e2700694ef40bad01c09b884660d2ffefc51
KimSoo <ilppang@naver.com>
2022-09-05 15:24:19 +0900

    bcrypt를 이용하여 비밀번호를 hash로 저장

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,12 +5,18 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthcredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthcredentialDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
     try {
       await this.save(user);
     } catch (error) {
