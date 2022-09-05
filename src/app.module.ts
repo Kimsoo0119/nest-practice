20fcef99148115f7fce7343bbd87c8b803dab797
KimSoo <ilppang@naver.com>
2022-09-05 12:46:30 +0900

    import 경로 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
-import { AuthController } from './—no-spec/auth/auth.controller';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
-    BoardsModule,
-    AuthModule],
-  controllers: [AuthController],  
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
+  controllers: [AuthController],
 })
 export class AppModule {}
