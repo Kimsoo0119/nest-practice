3394182f799a7ffc1e1f64ed4e585d17dee84cf2
KimSoo <ilppang@naver.com>
2022-09-05 15:11:57 +0900

    username unique 추가, unique 추가로 인한 에러 try catch 처리

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthcredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
@@ -7,7 +11,14 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthcredentialDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('이미 존재하는 username입니다.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
