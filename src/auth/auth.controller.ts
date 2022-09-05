67915a8e759b070315ed25945aa18391b3ce5a14
KimSoo <ilppang@naver.com>
2022-09-05 17:43:49 +0900

    UseGuaeds를 사용하여 req에 user 정보 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,7 @@
+import { UseGuards } from '@nestjs/common';
+import { Req } from '@nestjs/common';
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthcredentialDto } from './dto/auth-credential.dto';
 
@@ -18,4 +21,7 @@ export class AuthController {
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authcredentialDto);
   }
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@Req() req) {}
 }
